8931ad7b16c5a6a9cbcfbb6901b3a093c45515ff
Filipe Dantas Pereira <filipetecnico800@hotmail.com>
2020-01-06 19:27:24 -0300

    Redicionamento Correto na Autenticação

--- QuickBuy.Web/ClientApp/src/app/usuario/login/login.component.ts
+++ QuickBuy.Web/ClientApp/src/app/usuario/login/login.component.ts
@@ -1,22 +1,28 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { Usuario } from "../../modelo/usuario";
+import { Router, ActivatedRoute } from "@angular/router";
 
 @Component({
   selector: "app-login",
   templateUrl: "./login.component.html",
   styleUrls:["./login.component.css"]
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
   public usuario;
+  public returnlUrl: string;
 
-  constructor() {
+  constructor(private router: Router, private activatedRouter: ActivatedRoute) {
+  }
+
+  ngOnInit(): void {
+    this.returnlUrl = this.activatedRouter.snapshot.queryParams['returnUrl']
     this.usuario = new Usuario();
   }
- 
 
   entrar() {
-    if (this.usuario.email == "filipedantas.com" && this.usuario.senha == "abc123") {
-   
+    if (this.usuario.email == "filipe@dantas.com" && this.usuario.senha == "abc123") {
+      sessionStorage.setItem("usuario-autenticado", "1");
+      this.router.navigate([this.returnlUrl]);
     }
   }
 
